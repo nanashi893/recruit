c2b03c55f28a221ad7e69a937bc8d81d373e4c0a
soyozoramange2@gmail.com <soyozoramange2@gmail.com>
2024-10-15 18:05:08 +0900

    球の体積の問題を追加

--- recruit/main.cpp
+++ recruit/main.cpp
@@ -54,6 +54,13 @@ int main()
 		"この円錐の体積をXπcm^3とする。xの値を求めよ。",
 		x * x * y / 3});
 
+	// 球の体積
+	x = uniform_int_distribution<>(1, 5)(rand) * 3;
+	questions.push_back({
+		"球の半径" + to_string(x) + "cmの球がある。" +
+		"この球のの体積をXπcm^3とする。xの値を求めよ。",
+		x * x * x * 4 / 3 });
+
 
 	cout << "[リクルート試験対策クイズ]\n";
 
