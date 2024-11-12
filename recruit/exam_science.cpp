a89c51ae8fbe751ca1c3eb8dabe9885990020306
soyozoramange2@gmail.com <soyozoramange2@gmail.com>
2024-11-12 17:59:20 +0900

    理科問題の追加

--- recruit/exam_science.cpp
+++ recruit/exam_science.cpp
@@ -17,12 +17,20 @@ QuestionList CreatePhysicsExam()
 	{	//等速直線運動
 		constexpr int divisors[] = { 1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30, 60 };
 		int i = uniform_int_distribution<>(0, size(divisors) - 1)(rd);
-		int v = uniform_int_distribution<>(1, 10)(rd) * 60 / divisors[i]; // 速度
-		int t = uniform_int_distribution<>(1, 10)(rd) * divisors[i]; //
+		int v = uniform_int_distribution<>(1, 10)(rd) * 60 / divisors[i];	// 速度
+		int t = uniform_int_distribution<>(1, 10)(rd) * divisors[i];		// 時間
 		questions.push_back({
 			"時速" + to_string(v) + "kmで移動する車がある。\nこの車が" + to_string(v * t / 60) +
 			"km移動するのにかかる時間を分単位で求めよ",
 			to_string(t) });
+
+		i = uniform_int_distribution<>(0, size(divisors) - 1)(rd);
+		v = uniform_int_distribution<>(1, 10)(rd) * 60 / divisors[i];	// 速度
+		t = uniform_int_distribution<>(1, 10)(rd) * divisors[i];		// 時間
+		questions.push_back({
+			"ある車が" + to_string(v * t / 60) + "km離れた地点まで移動するのに" + to_string(t) +
+			"分かかった。\nこの車の平均時速を求めよ",
+			to_string(v) });
 	}//等速直線運動
 
 	return questions;
